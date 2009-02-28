be08c7b9fd21a825ab87de3fa36a839720fcec76
jonathan.schroder <jonathan.schroder@5e4e94e4-ed53-11dd-a550-c997509ed985>
2009-02-28 02:26:52 +0000

    the build tools folder with cool scripts for windows py2exe building..

--- build_tools/Win32setupNoLogin.py
+++ build_tools/Win32setupNoLogin.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
 import py2exe
-from os import system
+from os import system,path,getcwd
 import sys
 
 #without login info
 dir="Win32multi_tunnelerNoLogin"
-
-import sys
-sys.path.append(os.path.join(os.getcwd(),"..")) #makes the build_tools/../ folder's modules available...
+sys.path.append(path.normpath(path.join(sys.path[0],".."))) #makes the build_tools/../ folder's modules available...
                     #for importing and embedding into the final .exe
+
 setup(
     console=["multi_tunneler.py"],
     options={"py2exe":
@@ -18,4 +17,3 @@ setup(
     zipfile=None)
 
 system("copy tunnels_conf.json "+dir)
-sys.path.append(os.path.join(os.getcwd(),".."))
