be646849c84c30dcbc22cb1336ac7bf50f5a063b
jonathan.schroder <jonathan.schroder@5e4e94e4-ed53-11dd-a550-c997509ed985>
2009-02-28 02:26:52 +0000

    the build tools folder with cool scripts for windows py2exe building..

--- trunk/build_tools/Win32setupEmbeddedLogin.py
+++ trunk/build_tools/Win32setupEmbeddedLogin.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 import py2exe
-from os import system
+from os import system,path,getcwd
 import sys
 
 #without login info
 
 dir= "Win32multi_tunnelerEmbeddedLogin"
-sys.path.append(os.path.join(os.getcwd(),"..")) #makes the build_tools/../ folder's modules available...
+sys.path.append(path.normpath(path.join(sys.path[0],".."))) #makes the build_tools/../ folder's modules available...
                     #for importing and embedding into the final .exe
 setup(
     console=["multi_tunneler.py"],
